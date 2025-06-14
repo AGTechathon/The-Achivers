8ad0c1d1d23d62fc9e1c1303c76e27a857a1b124
sanjanamutkiri <sanjanamutkiri@gmail.com>
2025-06-15 04:49:39 +0530

    eniviroment setup

---
+++ mediscanAI/VideoAssistant/query_bakeoff.py
@@ -0,0 +1,2 @@
+## Multimodal demo
+# This is an example of how to simulate a video- and audio-aware model using existing LLM vision models (that take text and images as input, and generate text as output).
